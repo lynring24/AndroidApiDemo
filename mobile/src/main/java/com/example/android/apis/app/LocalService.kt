5f54bf2ef587dffab8b181f791ed948866ac7159
lyn <lync2846@gmail.com>
2017-08-27 02:26:15 +0900

    Import Context

--- mobile/src/main/java/com/example/android/apis/app/LocalService.kt
+++ mobile/src/main/java/com/example/android/apis/app/LocalService.kt
@@ -20,6 +20,7 @@ import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
